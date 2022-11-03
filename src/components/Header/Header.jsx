7ce92eebf79c42f95919c2acb0b500079e00fd74
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-03 22:02:54 +0100

    Header tools

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputeBase, Box  } from '@material-ui/core';
+import classNames from 'classnames';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar position='static'>
+      <Toolbar>
+        <Toolbar className={classNames.toolbar}>
+          <Typography variant='h5' className={classes.title}>
+            Travel Advisor
+          </Typography>
+          <Box display="flex">
+            <Typography variant='h6' className={classes.title}>
+            Explore New Places
+          </Typography>
+          </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
