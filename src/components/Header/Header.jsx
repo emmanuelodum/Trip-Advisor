c9dd26389cfa078b2ec74bb1fe2a1e734ae2bbac
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-04 21:16:15 +0100

    Autocomplete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputeBase, Box  } from '@material-ui/core';
-import classNames from 'classnames';
+import searchIcon from '@material-ui/icons';
 
 const Header = () => {
   return (
     <AppBar position='static'>
-      <Toolbar>
-        <Toolbar className={classNames.toolbar}>
+        <Toolbar className={classes.toolbar}>
           <Typography variant='h5' className={classes.title}>
             Travel Advisor
           </Typography>
           <Box display="flex">
             <Typography variant='h6' className={classes.title}>
-            Explore New Places
-          </Typography>
+              Explore New Places
+            </Typography>
+            <Autocomplete>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+              </div>
+              <InputeBase />
+              </div>
+            </Autocomplete>
           </Box>
-      </Toolbar>
+        </Toolbar>
     </AppBar>
   )
 }
