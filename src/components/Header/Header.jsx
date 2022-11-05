eb26213a1ee41d37b1a4f0e84c370c4bd5068414
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-05 21:45:25 +0100

    Placeholder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                 <div className={classes.searchIcon}>
                   <SearchIcon />
               </div>
-              <InputeBase />
+              <InputeBase  placeholder="search..." classes{{}} />
               </div>
             </Autocomplete>
           </Box>
