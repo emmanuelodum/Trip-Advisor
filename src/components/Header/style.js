0872d9caf420ce1592f38aedfd20f1fe8250989d
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-08 14:08:01 +0100

    Toolbar

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -15,7 +15,7 @@ export default makeStyles((theme) => ({
         marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
-        [theme.breakpoints.up('sm')]: {marginLeft: theme.spacing(3), width: 'auto'},
+        [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
     },
     searchIcon: {
         padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none',
@@ -26,4 +26,7 @@ export default makeStyles((theme) => ({
     inputInput: {
         padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     },
-}))
+    toolbar: {
+        display: 'flex', justifyContent: 'space-between',
+    },
+}));
