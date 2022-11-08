53c6bdc51ad184007a54767abc6434bc489a7d64
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-08 13:56:17 +0100

    SearchIcon styling

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -17,4 +17,7 @@ export default makeStyles((theme) => ({
         width: '100%',
         [theme.breakpoints.up('sm')]: {marginLeft: theme.spacing(3), width: 'auto'},
     },
+    searchIcon: {
+        padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none',
+    }
 }))
