8b7a85a9c8d3066fc2d5005f7b7f7c5cec8ea045
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-08 14:04:34 +0100

    Input root and input input

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -19,5 +19,11 @@ export default makeStyles((theme) => ({
     },
     searchIcon: {
         padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none',
-    }
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    },
 }))
