6db57259cb704c7508835a19b591f8bc142f0d33
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-08 13:52:14 +0100

    Search attributes

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,7 +1,20 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { alpha, makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
     title: {
-        color: 'red'
-    }
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.pallete.common.white, 0.15),
+        '&: hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {marginLeft: theme.spacing(3), width: 'auto'},
+    },
 }))
