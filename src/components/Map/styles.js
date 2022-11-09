06594b424c0191d2fc221fe0e6c0778c7e0d950d
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-09 17:12:05 +0100

    Map styles additionals

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -2,6 +2,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     paper: {
-        padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center'
-    }
-}))
+        padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '10px',
+    },
+    mapContainer: {
+        height: '85vh', width: '100%',
+    },
+    markerContainer: {
+        position: 'absolute', transform: 'translate(-50%, -50%)', '&:hover': { zIndex: '-1' },
+    },
+    pointer: {
+        cursor: 'pointer',
+    },
+}));
