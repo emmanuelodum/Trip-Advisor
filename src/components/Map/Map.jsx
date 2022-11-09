2d000ffee2b0fa150a5efb1c1360afd21c0ae5d3
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-09 17:03:57 +0100

    Map styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, usedMediaQuerry } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import  Rating  from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
   return (
---
+++ src/components/Map/styles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    paper: {
+        padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center'
+    }
+}))
