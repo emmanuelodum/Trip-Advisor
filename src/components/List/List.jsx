ff4cc1b8a675359b6f112af4e5b66a39764cf2af
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-19 21:39:36 +0100

    styles js list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { CircularProgress, Grid, Typograghy, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
+import useStyles from './styles'
+
 const List = () => {
   return (
     <div>List</div>
