d96fa940ae19518bf0ee99a738c1d5e62860e7dc
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-11-21 22:13:19 +0100

    useState call back functions

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CircularProgress, Grid, Typograghy, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 import useStyles from './styles'
@@ -6,12 +6,14 @@ import useStyles from './styles'
 const List = () => {
 
   const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+
   return (
     <div className={classes.container}>
       <Typograghy variant='h4'>Restaurants, Hotel & Attraction around you</Typograghy>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
-        <Select value={''} onChange={''}>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
           <MenuItem value="restuarants">Restaurants</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
           <MenuItem value="attractions">Attractions</MenuItem>
