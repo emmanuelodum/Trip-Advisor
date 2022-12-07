0caf40bf6f9d066fdc02bb6ee2ac2ee2fd89ca8f
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-12-07 15:13:07 +0100

    Api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,36 @@
 import axios from 'axios';
 
+const options = {
+  method: 'GET',
+  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'X-RapidAPI-Key': '1f6587bce3mshf3ca73812315c52p17f820jsn9fe7ab913c3f',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 const getPlacesData = async () => {
     try {
+
         const response = await axios.get();
 
 
